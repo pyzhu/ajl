acc5ec349d0663a3743206471e292e60b35f9911
Peter Zhu <pyz@Peters-iMac.local>
2017-02-25 23:01:57 -0500

    Add days incarcerated

--- R/getArr.R
+++ R/getArr.R
@@ -1,4 +1,4 @@
-getArr <- function(iter = 2000, n = 4000, a = .55, b = .2, mu = 0, sigma = .25) {
+getArr <- function(iter, n, a, b, mu, sigma, k, theta) {
   dt <- CJ(iter = seq.int(iter), arr_id = seq.int(n))
   setkey(dt, iter, arr_id)
   dt[, `:=`(true_risk = rbeta(.N, a, b)
@@ -7,6 +7,9 @@ getArr <- function(iter = 2000, n = 4000, a = .55, b = .2, mu = 0, sigma = .25) 
   # given the true risk, draw from binomial
   # TODO: would give false if true_risk = 1
   dt[, misbehave := runif(.N) < true_risk]
+  dt[, days_incarc := ceiling(rgamma(.N, shape = k, scale = theta))]
+  dt[, treated := as.numeric(sample(seq.int(.N) <= .N/2))]
+  dt[, psa_flag := as.numeric(sample(seq.int(.N) <= .N/2))]
 
   return(dt)
 }
--- R/simulate.R
+++ R/simulate.R
@@ -1,16 +1,24 @@
 #' @title Simulate
 #'
 #'
-simulate <- function(psa = 1, iter, n, a, b, mu, sigma, plot = TRUE) {
+simulate <- function(tau = 1, release.rate, iter, n, a, b, mu, sigma, k = .32, theta = 30, plot = TRUE) {
   judges <- getJudges(iter)
-  arrestees <- getArr(iter, n, a, b, mu, sigma)
-  arrestees[, judge_id := sample(judges$judge_id, size = length(arr_id), replace = TRUE)]
+  arrestees <- getArr(iter, n, a, b, mu, sigma, k = k, theta = theta)
+  # arrestees[, judge_id := sample(judges$judge_id, size = length(arr_id), replace = TRUE)]
+  dt <- data.table:::merge.data.table(CJ(arr_id = seq.int(n), tau = tau), arrestees, by="arr_id")
+  # dt <- merge(arrestees, judges[, list(iter, judge_id, bin, release, grp, psa_flag)], by=c("iter", "judge_id"))
+  # add in the scalars
+  dt[, tau := tau]
+  dt[, release_rate := release.rate]
 
-  dt <- merge(arrestees, judges[, list(iter, judge_id, bin, release, grp, psa_flag)], by=c("iter", "judge_id"))
-  dt[grp == "Half", psa_flag := as.numeric(runif(.N) < .5)]
-  dt[, obs_risk := true_risk + psa_flag * noise * psa + (1 - psa_flag) * noise]
-  dt[, decision := ecdf(obs_risk)(obs_risk) < release, by="iter,judge_id"]
-  dt[, psa := psa]
+  # dt[, treated := as.numeric(sample(seq.int(.N) <= .N/2))]
+  # dt[, psa_flag := as.numeric(sample(seq.int(.N) <= .N/2))]
+  dt[, obs_risk := true_risk + treated * noise * tau + (1 - treated) * noise]
+  dt[, released := ecdf(obs_risk)(obs_risk) <= release_rate, by="iter,treated"]
+
+  # add days incarcerated according to a gamma distribution
+  # using ceiling to get minimum 1 day
+  dt[, days_incarc := ceiling(rgamma(.N, shape = k, scale = theta))]
 
   return(dt)
 }
