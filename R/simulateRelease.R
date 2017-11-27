b1ffd646230584faebc22c763d1f084a71c02f98
jjsullivan4 <34005873+jjsullivan4@users.noreply.github.com>
2017-11-26 20:15:08 -0500

    Add control group

--- R/simulateRelease.R
+++ R/simulateRelease.R
@@ -5,14 +5,17 @@ simulateRelease <- function(tau = 1, release.rate, n, num.obs.nonwhite, a, b, mu
     , k = .32, theta = 77, haphazard = FALSE, plot = TRUE) {
   arrestees <- getArr(n, num.obs.nonwhite, a, b, mu, sigma, k = k, theta = theta)
 
-  # for each arrestee, there should be a number of tau values to assess
-  # where tau = 0, set treatment = 0 (definition of treatment)
-  # arr_id_treated <- arrestees[treated == 1, arr_id]
-  dt <- arrestees[CJ(arr_id, tau=tau)] # add the tau value and treated
-  dt[, treated := (tau > 0)]
-
+  # randomly assign half the arrestees to the control and the other half to the treatment group
+  arrestees[, treated := FALSE]
+  arrestees[sample(.N, floor(.N/2)), treated := TRUE]
+  
+  # add tau value to the treated arrestees only and merge treated arrestees with the non-treated arrestees
+  arr_id_treated <- arrestees[treated == 1, arr_id]
+  dt <- rbind(CJ(arr_id = arr_id_treated, tau = tau), arrestees[treated == 0, list(arr_id, tau = -1)], use.names = TRUE)
+  dt <- data.table:::merge.data.table(dt, arrestees, by="arr_id")
+    
   # make sure we have the right number of rows
-  assert_that(nrow(dt) == length(unique(arrestees$arr_id)) * length(tau))
+  assert_that(nrow(dt) == length(unique(arrestees$arr_id)) / 2 * (length(tau) + 1)
 
   # add in the scalars
   dt[, release_rate := release.rate]
